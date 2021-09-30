281dff8638993423947c9fc2ecf5f10a4c798fac
Maesro <maestrerodrigo@gmail.com>
2021-09-30 18:59:15 -0300

    Estructura finalizada. Falta modificar algunos textos/fotos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+function isMobile(){
+  return (
+      (navigator.userAgent.match(/Android/i)) ||
+      (navigator.userAgent.match(/webOS/i)) ||
+      (navigator.userAgent.match(/iPhone/i)) ||
+      (navigator.userAgent.match(/iPod/i)) ||
+      (navigator.userAgent.match(/iPad/i)) ||
+      (navigator.userAgent.match(/BlackBerry/i))
+  );
+}
+
+let ordenRedes="right"
+let delayRedes=1250
+let retardo=400
+
+if(isMobile()!=null){
+  ordenRedes="bottom"
+  delayRedes=1050
+  retardo=200
+} 
+
 ScrollReveal().reveal(".mov1", {
   duration: 1000,
   origin: "left",
@@ -36,33 +57,32 @@ ScrollReveal().reveal(".mov5", {
 });
 ScrollReveal().reveal(".mov6", {
   duration: 1000,
-  origin: "right",
+  origin: ordenRedes,
   distance: "400px",
   reset: false,
-  delay: 1500,
+  delay: delayRedes+retardo,
 });
 ScrollReveal().reveal(".mov7", {
   duration: 1000,
-  origin: "right",
+  origin: ordenRedes,
   distance: "400px",
   reset: false,
-  delay: 2000,
+  delay: delayRedes+(2*retardo),
 });
 ScrollReveal().reveal(".mov8", {
   duration: 1000,
-  origin: "right",
+  origin: ordenRedes,
   distance: "400px",
   reset: false,
-  delay: 2500,
+  delay:  delayRedes+(3*retardo),
 });
-
 
 ScrollReveal().reveal(".mov9", {
   duration: 1000,
-  origin: "right",
+  origin: ordenRedes,
   distance: "400px",
   reset: false,
-  delay: 3000,
+  delay:  delayRedes+(4*retardo),
 });
 
 $("#menu").hide();
@@ -92,20 +112,24 @@ $(".navmob_JZ").click(()=>{
 })
 
 $("#proy02").hide();
-$(".proy__btn__vuelta").hide();
+$(".proy__btn__vueltaImg").hide();
+$(".proy__btn__irImg-bloque").hide();
+$(".proy__btn__vueltaImg-bloque").show();
 
-
-
-$(".proy__btn__ir").click(()=>{
+$(".proy__btn__irImg").click(()=>{
   $("#proy01").hide(0);
   $("#proy02").show(0);
-  $(".proy__btn__ir").hide();
-  $(".proy__btn__vuelta").show();
+  $(".proy__btn__irImg").hide();
+  $(".proy__btn__vueltaImg").show();
+  $(".proy__btn__vueltaImg-bloque").hide();
+  $(".proy__btn__irImg-bloque").show();
 }) 
-$(".proy__btn__vuelta").click(()=>{
+$(".proy__btn__vueltaImg").click(()=>{
   $("#proy02").hide(0);
   $("#proy01").show(0);
-  $(".proy__btn__ir").show();
-  $(".proy__btn__vuelta").hide();
+  $(".proy__btn__irImg").show();
+  $(".proy__btn__vueltaImg").hide();
+  $(".proy__btn__irImg-bloque").hide();
+  $(".proy__btn__vueltaImg-bloque").show();
 }) 
   
